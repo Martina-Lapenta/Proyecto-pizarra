3e17c20676296c1c32e09c10a1c9c35049456b8f
martina lapenta <martinalapenta12@gmail.com>
2023-10-31 22:45:14 -0300

    Mensaje descriptivo de los cambios

--- servidor/server.js
+++ servidor/server.js
@@ -15,8 +15,48 @@ server.listen (PORT,()=> {
     console.log("Servidor escuchando en el puerto 3000");
 });
 
+const jugadores = [];
+
+// ...
+
+wss.on("connection", (ws) => {
+    console.log("Se conectó el ws con éxito");
+
+    ws.on("message", (message) => {
+        const data = JSON.parse(message);
+        console.log("Mensaje recibido del front", data);
+        ws.send(JSON.stringify(data));
+
+        if (data.tipo === 'nombreJugador') {
+            jugadores.push(data.Playername);
+            // Envía la lista actualizada de jugadores a todos los clientes
+            wss.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({ tipo: 'jugadores', jugadores }));
+                }
+            });
+        }
+        
+        if (data.tipo === 'dibujo') {
+            // Reenvía el mensaje a todos los clientes conectados
+            wss.clients.forEach((client) => {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify(data));
+                }
+            });
+        }
+    });
+
+    ws.on("close", (event) => {
+        console.log("Se desconectó ", event);
+    });
+});
+
+// ...
+
+
 //estructura de websocket
-wss.on ("connection",(ws)=>{
+/*wss.on ("connection",(ws)=>{
     console.log ("Se conecto el ws con exito");
     ws.on ("message", (message) => {
         const data = JSON.parse(message);
@@ -26,4 +66,5 @@ wss.on ("connection",(ws)=>{
     ws.on ("close", (event) => {
         console.log ("Se desconecto ", event);
     });
-});
+
+});*/
